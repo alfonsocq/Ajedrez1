c7e4e0790a945f8d80ca003380509f2c69a147ba
alfonsocq <alfcastroq@gmail.com>
2022-03-11 15:59:11 -0500

    ajedrez1

--- server.py
+++ server.py
@@ -2,11 +2,25 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')                           
-def hola_mundo():
+def principal():
     return render_template('index.html')  
     
-if __name__=="__main__":
-    app.run(debug=True) 
+
 #def four():
 #  return "4"
 #@app.route('/(x)/(y)'
+
+@app.route('/<int:x>')                           
+def personal(x):
+    return render_template("unvalor.html", x=x)  
+
+@app.route('/4')                           
+def cuatro():
+    return render_template('cuatro.html')  
+
+@app.route('/<int:x>/<int:y>')                           
+def tableropersonal(x,y):
+    return render_template("tableropersonal.html", x=x, y=y)  
+
+if __name__=="__main__":
+    app.run(debug=True) 
